4beb074de21b64388350d25fae7d7bf940d18e27
github-action-benchmark <github@users.noreply.github.com>
2024-01-23 20:07:02 +0000

    add Arena Benchmark (customBiggerIsBetter) benchmark result for 64579c8cc143d6c234023f4236756c0a04046903

--- benchmarking/arena/data.js
+++ benchmarking/arena/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706040155137,
+  "lastUpdate": 1706040422887,
   "repoUrl": "https://github.com/ruse-ai/mitpokerbots-2024",
   "entries": {
     "Arena Benchmark": [
@@ -16,91 +16,91 @@ window.BENCHMARK_DATA = {
             "username": "philqc"
           },
           "distinct": true,
-          "id": "0bfb609fd0f80344c8f697c6f9b767af288773e5",
+          "id": "ed0daa3f44e8dbfc4c6092d532a10fc4811c92b3",
+          "message": "max items to clear chart",
+          "timestamp": "2024-01-23T14:58:21-05:00",
+          "tree_id": "7a666c9d879ddb973380b388c4fe62be17ff4775",
+          "url": "https://github.com/ruse-ai/mitpokerbots-2024/commit/ed0daa3f44e8dbfc4c6092d532a10fc4811c92b3"
+        },
+        "date": 1706040152857,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Results vs. Check/Call Bot",
+            "value": 0,
+            "unit": "Big blinds"
+          },
+          {
+            "name": "Results vs. Bid Everything Bot",
+            "value": 56.93,
+            "unit": "BB/100"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "philbeardsell@gmail.com",
+            "name": "Philippe Beardsell",
+            "username": "philqc"
+          },
+          "committer": {
+            "email": "philbeardsell@gmail.com",
+            "name": "Philippe Beardsell",
+            "username": "philqc"
+          },
+          "distinct": true,
+          "id": "ed0daa3f44e8dbfc4c6092d532a10fc4811c92b3",
+          "message": "max items to clear chart",
+          "timestamp": "2024-01-23T14:58:21-05:00",
+          "tree_id": "7a666c9d879ddb973380b388c4fe62be17ff4775",
+          "url": "https://github.com/ruse-ai/mitpokerbots-2024/commit/ed0daa3f44e8dbfc4c6092d532a10fc4811c92b3"
+        },
+        "date": 1706040152857,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Results vs. Check/Call Bot",
+            "value": 0,
+            "unit": "Big blinds"
+          },
+          {
+            "name": "Results vs. Bid Everything Bot",
+            "value": 56.93,
+            "unit": "BB/100"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "philbeardsell@gmail.com",
+            "name": "Philippe Beardsell",
+            "username": "philqc"
+          },
+          "committer": {
+            "email": "philbeardsell@gmail.com",
+            "name": "Philippe Beardsell",
+            "username": "philqc"
+          },
+          "distinct": true,
+          "id": "64579c8cc143d6c234023f4236756c0a04046903",
           "message": "fix",
-          "timestamp": "2024-01-23T14:46:03-05:00",
-          "tree_id": "163d8dc94328c0a242156ef263d66cb39d48a231",
-          "url": "https://github.com/ruse-ai/mitpokerbots-2024/commit/0bfb609fd0f80344c8f697c6f9b767af288773e5"
+          "timestamp": "2024-01-23T15:04:15-05:00",
+          "tree_id": "9d6eba6ea00470b5ed269dc58bfeabc44a553377",
+          "url": "https://github.com/ruse-ai/mitpokerbots-2024/commit/64579c8cc143d6c234023f4236756c0a04046903"
         },
-        "date": 1706039666130,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "Results vs. Check/Call Bot",
-            "value": -42.04,
-            "unit": "Big blinds"
-          },
-          {
-            "name": "Results vs. Bid Everything Bot",
-            "value": 110.37,
-            "unit": "BB/100"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "philbeardsell@gmail.com",
-            "name": "Philippe Beardsell",
-            "username": "philqc"
-          },
-          "committer": {
-            "email": "philbeardsell@gmail.com",
-            "name": "Philippe Beardsell",
-            "username": "philqc"
-          },
-          "distinct": true,
-          "id": "ed0daa3f44e8dbfc4c6092d532a10fc4811c92b3",
-          "message": "max items to clear chart",
-          "timestamp": "2024-01-23T14:58:21-05:00",
-          "tree_id": "7a666c9d879ddb973380b388c4fe62be17ff4775",
-          "url": "https://github.com/ruse-ai/mitpokerbots-2024/commit/ed0daa3f44e8dbfc4c6092d532a10fc4811c92b3"
-        },
-        "date": 1706040152857,
+        "date": 1706040421858,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
             "name": "Results vs. Check/Call Bot",
             "value": 0,
-            "unit": "Big blinds"
-          },
-          {
-            "name": "Results vs. Bid Everything Bot",
-            "value": 56.93,
             "unit": "BB/100"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "philbeardsell@gmail.com",
-            "name": "Philippe Beardsell",
-            "username": "philqc"
-          },
-          "committer": {
-            "email": "philbeardsell@gmail.com",
-            "name": "Philippe Beardsell",
-            "username": "philqc"
-          },
-          "distinct": true,
-          "id": "ed0daa3f44e8dbfc4c6092d532a10fc4811c92b3",
-          "message": "max items to clear chart",
-          "timestamp": "2024-01-23T14:58:21-05:00",
-          "tree_id": "7a666c9d879ddb973380b388c4fe62be17ff4775",
-          "url": "https://github.com/ruse-ai/mitpokerbots-2024/commit/ed0daa3f44e8dbfc4c6092d532a10fc4811c92b3"
-        },
-        "date": 1706040152857,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "Results vs. Check/Call Bot",
-            "value": 0,
-            "unit": "Big blinds"
           },
           {
             "name": "Results vs. Bid Everything Bot",
-            "value": 56.93,
+            "value": -228.15,
             "unit": "BB/100"
           }
         ]
