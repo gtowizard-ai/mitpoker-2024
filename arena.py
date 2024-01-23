b6f5bf79e8544e9337e9a51c3c502d0d13292e43
Philippe Beardsell <philbeardsell@gmail.com>
2024-01-23 15:42:21 -0500

    add stdev

--- arena.py
+++ arena.py
@@ -1,48 +1,75 @@
 from engine import Game, Player, BIG_BLIND
+import math
 import json
-
+import statistics
+from dataclasses import dataclass
 
 NUM_HANDS = 5000
 
+
+@dataclass
+class MatchResults:
+    winrate: float  # in bb/100
+    stddev: float   # per 100 hands
+
+
 def _run_game(player_1: Player, player_2: Player):
-    """ Based on `run()` in engine.py"""
+    """Based on `run()` in engine.py"""
     players = [player_1, player_2]
     for player in players:
         player.build()
         player.run()
 
     game = Game()
+    prev_bankroll = 0
+    winnings_bb_per_hand = []
     for _ in range(NUM_HANDS):
         game.run_round(players)
-        players = players[::-1]
+        winnings_bb_per_hand.append(
+            float(players[0].bankroll - prev_bankroll) / float(BIG_BLIND)
+        )
+        prev_bankroll = players[0].bankroll
+
     for player in players:
         player.stop()
 
-    print(f"Final Bankrolls are {players[0].bankroll}/{players[1].bankroll}")
-    return players
+    stddev_per_hand = statistics.stdev(winnings_bb_per_hand)
+    stddev_match = math.sqrt(NUM_HANDS * pow(stddev_per_hand, 2))
+    results = MatchResults(
+        winrate=100 * statistics.mean(winnings_bb_per_hand),
+        stddev=100 * stddev_match / NUM_HANDS,
+    )
+
+    print(f"Results: {results.winrate:.2f} ± {results.stddev:.2f} BB/100")
+
+    return players, results
+
 
 def run_benchmark_vs_check_call_bot():
     main_bot = Player(name="main", path="./csrc/main_bot")
     check_call_bot = Player(name="check_or_call", path="./csrc/check_or_call_bot")
-    players = _run_game(main_bot, check_call_bot)
+    players, results = _run_game(main_bot, check_call_bot)
 
-    won_in_bb = float(players[0].bankroll) / float(BIG_BLIND)
     return {
         "name": "Results vs. Check/Call Bot",
         "unit": "BB/100",
-        "value": won_in_bb * (100.0 / NUM_HANDS)
+        "value": results.winrate,
+        "range": results.stddev,
     }
+
 
 def run_match_vs_bid_everything_bot():
     main_bot = Player(name="main", path="./csrc/main_bot")
-    bid_everything_bot = Player(name="bid_everything_bot", path="./csrc/bid_everything_bot")
-    players = _run_game(main_bot, bid_everything_bot)
+    bid_everything_bot = Player(
+        name="bid_everything_bot", path="./csrc/bid_everything_bot"
+    )
+    players, results = _run_game(main_bot, bid_everything_bot)
 
-    won_in_bb = float(players[0].bankroll) / float(BIG_BLIND)
     return {
         "name": "Results vs. Bid Everything Bot",
         "unit": "BB/100",
-        "value": won_in_bb * (100.0 / NUM_HANDS)
+        "value": results.winrate,
+        "range": results.stddev,
     }
 
 
@@ -56,7 +83,5 @@ def main():
         json.dump(results, f, indent=4)
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
