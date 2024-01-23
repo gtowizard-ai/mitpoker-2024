2cfcc838d8b1d77bc00c84237d4bf1cd616c2006
github-action-benchmark <github@users.noreply.github.com>
2024-01-23 18:40:04 +0000

    add Arena Benchmark (customBiggerIsBetter) benchmark result for c340cd268ad7bd92049016516df0faa239080937

---
+++ benchmarking/arena/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1706035204194,
+  "repoUrl": "https://github.com/ruse-ai/mitpokerbots-2024",
+  "entries": {
+    "Arena Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "philbeardsell@gmail.com",
+            "name": "Philippe Beardsell",
+            "username": "philqc"
+          },
+          "committer": {
+            "email": "philbeardsell@gmail.com",
+            "name": "Philippe Beardsell",
+            "username": "philqc"
+          },
+          "distinct": true,
+          "id": "c340cd268ad7bd92049016516df0faa239080937",
+          "message": "wip - arena workflow",
+          "timestamp": "2024-01-23T13:28:11-05:00",
+          "tree_id": "f7de9e6f64a337ec86c8c4f7cef0f7df2c3b98e3",
+          "url": "https://github.com/ruse-ai/mitpokerbots-2024/commit/c340cd268ad7bd92049016516df0faa239080937"
+        },
+        "date": 1706035203196,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Results vs. Check/Call Player",
+            "value": 0,
+            "unit": "Big blinds"
+          }
+        ]
+      }
+    ]
+  }
+}
