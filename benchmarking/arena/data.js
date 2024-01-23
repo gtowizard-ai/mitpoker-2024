b610ff308ea64172e41089b1690ed6e3438127d7
github-action-benchmark <github@users.noreply.github.com>
2024-01-23 20:07:04 +0000

    add Arena Benchmark (customBiggerIsBetter) benchmark result for 64579c8cc143d6c234023f4236756c0a04046903

--- benchmarking/arena/data.js
+++ benchmarking/arena/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706040422887,
+  "lastUpdate": 1706040424275,
   "repoUrl": "https://github.com/ruse-ai/mitpokerbots-2024",
   "entries": {
     "Arena Benchmark": [
@@ -50,23 +50,23 @@ window.BENCHMARK_DATA = {
             "username": "philqc"
           },
           "distinct": true,
-          "id": "ed0daa3f44e8dbfc4c6092d532a10fc4811c92b3",
-          "message": "max items to clear chart",
-          "timestamp": "2024-01-23T14:58:21-05:00",
-          "tree_id": "7a666c9d879ddb973380b388c4fe62be17ff4775",
-          "url": "https://github.com/ruse-ai/mitpokerbots-2024/commit/ed0daa3f44e8dbfc4c6092d532a10fc4811c92b3"
+          "id": "64579c8cc143d6c234023f4236756c0a04046903",
+          "message": "fix",
+          "timestamp": "2024-01-23T15:04:15-05:00",
+          "tree_id": "9d6eba6ea00470b5ed269dc58bfeabc44a553377",
+          "url": "https://github.com/ruse-ai/mitpokerbots-2024/commit/64579c8cc143d6c234023f4236756c0a04046903"
         },
-        "date": 1706040152857,
+        "date": 1706040421858,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
             "name": "Results vs. Check/Call Bot",
             "value": 0,
-            "unit": "Big blinds"
+            "unit": "BB/100"
           },
           {
             "name": "Results vs. Bid Everything Bot",
-            "value": 56.93,
+            "value": -228.15,
             "unit": "BB/100"
           }
         ]
