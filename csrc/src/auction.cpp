f18314ebc02f237b105e42a7c297605210665431
GrahamZugPitt <GVZ3@pitt.edu>
2024-01-25 11:59:15 -0500

    addressing Phil's comments

--- csrc/src/auction.cpp
+++ csrc/src/auction.cpp
@@ -1,12 +1,12 @@
 #include "auction.h"
+#include <iostream>
 
 namespace pokerbot {
 
 Auctioneer::Auctioneer(){};
 
 int Auctioneer::get_bid(const std::vector<Range>& ranges, const std::vector<Card>& board,
-                        Hand& hand, float time_budget) {
-  std::cout << "I bid" << std::endl;
+                        const Hand& hand, float time_budget) {
   return 0;
 }
 }  // namespace pokerbot
