8d7753ea044460e5bf46a83a2424b805e3766188
Wataru Inariba <oinari17@gmail.com>
2024-01-26 10:30:36 +0900

    fix range test

--- csrc/src/range_test.cpp
+++ csrc/src/range_test.cpp
@@ -52,12 +52,9 @@ TEST_F(RangeTest, TestTo3CardsRangeWithBoardCards) {
   Range range;
   ASSERT_EQ(range.num_cards, NumCards::Two);
 
+  range.to_3_cards_range(game_, board_cards);
   range.update_on_new_board_cards(game_, board_cards);
 
-  const auto two_cards_sum = ranges::sum(range.range);
-  ASSERT_NEAR(two_cards_sum, n_choose_k(48, 2), TOLERANCE);
-
-  range.to_3_cards_range(game_, board_cards);
   ASSERT_EQ(range.num_cards, NumCards::Three);
-  ASSERT_NEAR(ranges::sum(range.range), two_cards_sum, TOLERANCE);
+  ASSERT_NEAR(ranges::sum(range.range), n_choose_k(48, 2), TOLERANCE);
 }
