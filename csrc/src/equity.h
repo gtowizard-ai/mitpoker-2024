a8dad227397c02c5c5953cd2c31d7d038d170e52
Philippe Beardsell <philbeardsell@gmail.com>
2024-01-26 09:42:15 -0500

    Equity optimization no highway (#25)
    * Use `VQSort` instead of `std::sort`
    
    * Remove validations in `PokerHand` (30%~ faster)
    
    * Fix `PokerHand` test
    
    * Optimize river equity
    
    * Tentative fix for Clang
    
    * Fix
    
    * no highway
    
    ---------
    
    Co-authored-by: Wataru Inariba <oinari17@gmail.com>

--- csrc/src/equity.h
+++ csrc/src/equity.h
@@ -3,11 +3,19 @@
 #include "poker_hand.h"
 #include "range.h"
 
+#include <type_traits>
+
 namespace pokerbot {
 
-/// Computes the counterfactual values and stores them in `cfvs`.
+/// Computes the counterfactual values and adds them to the `cfvs` vector.
+/// @param cfvs A vector of size `hero_range.num_hands()`. Note that the results are added to this
+/// vector, not overwritten. The type `T` must be a floating-point type.
+/// @param is_river_equity If `true`, the `cfvs` vector will be converted to a vector of equities.
+/// In this case, the `payoff` must be 0.5.
+template <typename T, typename = std::enable_if_t<std::is_floating_point_v<T>>>
 void compute_cfvs_river(const Game& game, const Range& hero_range, const Range& opponent_range,
-                        const PokerHand& board, std::vector<double>& cfvs, double payoff);
+                        const PokerHand& board, std::vector<T>& cfvs, double payoff,
+                        bool is_river_equity = false);
 
 /// Computes the equities of the hero range against the opponent range.
 /// @return A vector of size `hero_range.num_hands()`.
