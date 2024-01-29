aae47f28729c4ba38e1b867940ce41dcae7eafcf
Philippe Beardsell <philbeardsell@gmail.com>
2024-01-29 09:19:38 -0500

    add cushion time manager

--- csrc/src/time_manager.h
+++ csrc/src/time_manager.h
@@ -15,7 +15,9 @@ class TimeManager {
         num_actions_taken_ < 50 ? 6 : static_cast<double>(num_actions_taken_) / game_info.hand_num;
     double num_actions_left_match = avg_num_actions_per_hand * game_info.num_hands_left_in_match();
 
-    return (1000.0 * game_info.game_clock) / num_actions_left_match;
+    // Add epsilon to make sure we don't bust the limit
+    double time_left_seconds = game_info.game_clock + 0.5;
+    return (1000.0 * time_left_seconds) / num_actions_left_match;
   }
 
  private:
