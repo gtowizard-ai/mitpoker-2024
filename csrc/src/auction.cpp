db064b647e32841730700853297db73bb89095e1
GrahamZugPitt <46653792+GrahamZugPitt@users.noreply.github.com>
2024-01-30 11:13:57 -0500

    Exploits (#36)
    * initial get_bid commit
    
    * Added equity based bidding
    
    * snake to camel
    
    * Fixed arithmetic mistake
    
    * checkpoint
    
    * Removed old slow bidding
    
    * snakkkkeeee
    
    * Aggro bidding
    
    * REALLY solved merge conflict
    
    * Fixed test
    
    * Removed flop value generator
    
    * Removed extra import
    
    * Removed extra import... again!
    
    * removed useless code
    
    * Removed second +1
    
    * removed extra +1

--- csrc/src/auction.cpp
+++ csrc/src/auction.cpp
@@ -1,16 +1,11 @@
 #include "auction.h"
+#include <numeric>
 #include "../scripts/avg_equity_third_card.h"
 #include "definitions.h"
 #include "equity.h"
 #include "isomorphic_flop_encoder.h"
 
 namespace pokerbot {
-
-inline constexpr int ABS_BIDDING_EPSILON = 2;
-inline constexpr float POT_PERCENTAGE_BIDDING_EPSILON = .1;
-inline constexpr int REASONABLE_DIST_FROM_MAX = 10;
-inline constexpr float BID_MULTIPLIER_OOP = 1;
-inline constexpr float BID_MULTIPLIER_IP = 1;
 
 Auctioneer::Auctioneer() {
   v_is_excessive_bidder = true;
@@ -26,22 +21,47 @@ float Auctioneer::get_avg_equity_third_card(const std::vector<card_t>& board_car
   return -equity_lost;
 }
 
+float Auctioneer::mean_equity(const Range& range_one, const Range& range_two, const Game& game,
+                              const std::vector<card_t>& board) {
+
+  std::vector<float> eq = compute_equities(game, range_one, range_two, board);
+  float sum = std::accumulate(eq.begin(), eq.end(), 0.0);
+  return eq.empty() ? 0.0 : sum / eq.size();
+}
+
 int Auctioneer::get_bid(const Range& hero_range, const Range& villain_range, const Game& game,
-                        const std::vector<card_t>& board_cards, const Hand& hand, const int pot,
+                        const std::vector<card_t>& board, const Hand& hand, const int pot,
                         float time_budget_ms) {
-  //TODO: Implement exploitative bidding based on previous received values
-  // Range hero_three_card = hero_range;
-  // hero_three_card.to_3_cards_range(game, board_cards);
-  //
-  // Range villain_three_card = villain_range;
-  // villain_three_card.to_3_cards_range(game, board_cards);
-  //TODO: Implement time efficient way for calculating equity
-  //TODO: Add cohesive get_bid test to testing
 
-  float equity_difference = get_avg_equity_third_card(board_cards);
-  float equity_bid = ((1 / (1 - equity_difference)) - 1) * pot;
+  float equity_difference = get_avg_equity_third_card(board);
 
-  return std::ceil(equity_bid);
+  int abs_bid_diff = v_abs_bid_min_max[1] - v_abs_bid_min_max[0];
+  float rel_bid_diff = v_pot_percentage_min_max[1] - v_pot_percentage_min_max[0];
+
+  //This is the sum of the geometric series of of form
+  //x + x^2 + x^3..., which accounts for us getting the equity
+  //back from our bid itself
+  float equity_with_bid = ((1 / (1 - equity_difference)) - 1) * pot;
+  int default_bid = std::ceil(equity_with_bid) + 1;
+
+  if (SIGNIFICANT_BID_COUNT > bid_count) {
+    return default_bid;
+  }
+  if (v_is_excessive_bidder) {
+    int stack = STARTING_STACK - (pot / 2);
+    return stack - REASONABLE_DIST_FROM_MAX;
+  }
+  if (std::abs(abs_bid_diff) < ABS_BIDDING_EPSILON) {
+    std::vector<int> bids = {default_bid, v_abs_bid_min_max[0]};
+    return *std::max_element(bids.begin(), bids.end());
+  }
+  if (std::abs(abs_bid_diff) < POT_PERCENTAGE_BIDDING_EPSILON) {
+    //We floor here because we are trying to undercut villain
+    std::vector<int> bids = {default_bid,
+                             static_cast<int>(std::floor(v_pot_percentage_min_max[0] * pot))};
+    return *std::max_element(bids.begin(), bids.end());
+  }
+  return default_bid;
 }
 
 void Auctioneer::update_exploits(const int bid, const int pot) {
@@ -63,6 +83,7 @@ void Auctioneer::update_exploits(const int bid, const int pot) {
   if (bid_to_pot > v_pot_percentage_min_max[1]) {
     v_pot_percentage_min_max[1] = bid_to_pot;
   }
+  bid_count++;
 }
 
 void Auctioneer::receive_bid(Range& hero_range, Range& villain_range, const int hero_bid,
--- csrc/src/auction.h
+++ csrc/src/auction.h
@@ -5,6 +5,13 @@
 
 namespace pokerbot {
 
+inline constexpr int ABS_BIDDING_EPSILON = 2;
+inline constexpr float POT_PERCENTAGE_BIDDING_EPSILON = .1;
+inline constexpr int REASONABLE_DIST_FROM_MAX = 10;
+inline constexpr float BID_MULTIPLIER_OOP = 1;
+inline constexpr float BID_MULTIPLIER_IP = 1;
+inline constexpr int SIGNIFICANT_BID_COUNT = 5;
+
 struct Auctioneer {
 
   Auctioneer();
@@ -12,11 +19,14 @@ struct Auctioneer {
   int v_abs_bid_min_max[2];
   float v_pot_percentage_min_max[2];
   bool v_is_excessive_bidder;
+  int bid_count = 0;
 
   int get_bid(const Range& hero_range, const Range& villain_range, const Game& game,
               const std::vector<card_t>& board_cards, const Hand& hand, int pot,
               float time_budget_ms);
 
+  float mean_equity(const Range& range_one, const Range& range_two, const Game& game,
+                    const std::vector<card_t>& board);
   float get_avg_equity_third_card(const std::vector<card_t>& board_cards);
 
   void receive_bid(Range& hero_range, Range& villain_range, int hero_bid, int villain_bid,
