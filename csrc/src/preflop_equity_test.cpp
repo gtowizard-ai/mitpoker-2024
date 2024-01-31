b45e59922d3ecb1212d420e64fbb8eab46578db5
Philippe Beardsell <philbeardsell@gmail.com>
2024-01-30 21:46:25 -0500

    delete eqs - not needed

--- csrc/src/preflop_equity_test.cpp
+++ csrc/src/preflop_equity_test.cpp
@@ -13,11 +13,6 @@ TEST(PreflopEquityTest, TestIntegrityValues) {
     return std::isfinite(idx) && idx >= 0 && idx < NUM_HANDS_PREFLOP;
   }));
 
-  ASSERT_EQ(PREFLOP_EQUITIES.size(), NUM_HANDS_PREFLOP * NUM_HANDS_PREFLOP);
-  ASSERT_TRUE(std::all_of(PREFLOP_EQUITIES.begin(), PREFLOP_EQUITIES.end(), [](auto equity) {
-    return std::isfinite(equity) && equity >= 0 && equity <= 1.0;
-  }));
-
   ASSERT_EQ(PREFLOP_PAYOFFS.size(), NUM_HANDS_PREFLOP * NUM_HANDS_PREFLOP);
   ASSERT_TRUE(std::all_of(PREFLOP_PAYOFFS.begin(), PREFLOP_PAYOFFS.end(), [](auto payoff) {
     return std::isfinite(payoff) && payoff >= -1.0 && payoff <= 1.0;
@@ -26,14 +21,6 @@ TEST(PreflopEquityTest, TestIntegrityValues) {
   auto idx_22 = PREFLOP_HAND_IDX[Hand("2c2d").index()];
   auto idx_32 = PREFLOP_HAND_IDX[Hand("3c2d").index()];
   auto idx_aa = PREFLOP_HAND_IDX[Hand("AcAd").index()];
-
-  ASSERT_NEAR(PREFLOP_EQUITIES[idx_22 * NUM_HANDS_PREFLOP + idx_22], 0.5, 1e-4);
-  ASSERT_NEAR(PREFLOP_EQUITIES[idx_32 * NUM_HANDS_PREFLOP + idx_32], 0.5, 1e-4);
-  ASSERT_NEAR(PREFLOP_EQUITIES[idx_aa * NUM_HANDS_PREFLOP + idx_aa], 0.5, 1e-4);
-  ASSERT_NEAR(PREFLOP_EQUITIES[idx_22 * NUM_HANDS_PREFLOP + idx_32], 0.6658, 1e-4);
-  ASSERT_NEAR(PREFLOP_EQUITIES[idx_32 * NUM_HANDS_PREFLOP + idx_22], 0.3342, 1e-4);
-  ASSERT_NEAR(PREFLOP_EQUITIES[idx_32 * NUM_HANDS_PREFLOP + idx_aa], 0.1279, 1e-4);
-  ASSERT_NEAR(PREFLOP_EQUITIES[idx_aa * NUM_HANDS_PREFLOP + idx_32], 0.8721, 1e-4);
 
   ASSERT_NEAR(PREFLOP_PAYOFFS[idx_22 * NUM_HANDS_PREFLOP + idx_22], 0, 1e-4);
   ASSERT_NEAR(PREFLOP_PAYOFFS[idx_32 * NUM_HANDS_PREFLOP + idx_32], 0, 1e-4);
